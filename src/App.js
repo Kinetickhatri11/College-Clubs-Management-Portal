9eed2faa0bb6e09d379e2ade5ab9eda938436ac0
nikita08Singh <singhnikita7676@gmail.com>
2023-05-23 17:35:24 +0530

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
-import Home from './components/pages/Home'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../src/components/pages/Home";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <>
     <Router>
-      <Navbar/>
-      <Routes>
-        <Route path="/" exact component={Home}/>
-      </Routes>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Fragment>
     </Router>
-    </>
   );
 }
 
