7be497eb64a57a32a13af6ee9123399be2daecb1
Shuklashashi0706 <shuklashashi169@gmail.com>
2023-05-23 19:13:04 +0530

    Created Cards Component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,12 +2,13 @@ import React from 'react';
 import '../../src/components/Main.css';
 
 import HeroSection from '../components/Home/HeroSection';
-
+import Cards from '../components/Cards/Cards'
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards/>
     </>
   );
 }
